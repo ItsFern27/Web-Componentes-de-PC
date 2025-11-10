4ec8d8e35960f33268f5f318b609da5bf6894e78
ItsFern27 <fjarevalo07@gmail.com>
2025-11-09 19:32:38 -0500

    inicio 4

--- src/main/java/com/pccore/pccore/config/SecurityConfig.java
+++ src/main/java/com/pccore/pccore/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
