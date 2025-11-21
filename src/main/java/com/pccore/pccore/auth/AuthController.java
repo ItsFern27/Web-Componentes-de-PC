02a5b57dc873689a6fa882ccd3b6b13ae5e435cd
cristhian <cristhianbrh@gmail.com>
2025-11-21 12:38:56 -0500

    Se realizo la alerta en un modal

--- src/main/java/com/pccore/pccore/auth/AuthController.java
+++ src/main/java/com/pccore/pccore/auth/AuthController.java
@@ -1,12 +1,12 @@
 package com.pccore.pccore.auth;
 
+import java.util.Map;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
